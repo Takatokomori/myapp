ae0c5a3cdf1211a930322a17a143177753c5c04f
takatokomori <ktakato510@gmail.com>
2022-10-29 15:55:33 +0900

    todolist

---
+++ src/components/InputField.tsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import "./style.css";
+
+
+interface Props{
+  todo:string;
+  setTodo:React.Dispatch<React.SetStateAction<string>>;
+  handleAdd:(e: React.FormEvent) => void;
+}
+
+export default function InputField({todo, setTodo, handleAdd}: Props) {
+  return (
+    <form className='input' onSubmit={handleAdd} >
+      <input type="input" 
+       placeholder='Enter a task'
+       className='input_box'
+       value={todo}
+       onChange = {(e) => setTodo(e.target.value)} />
+      <button className='input_submit' 
+       type='submit'>GO</button>
+    </form>
+  )
+}
